11ec872c2e3859f6a39dc1d62ada9c6818b735c5
cerodolDev <spljifsdev@gmail.com>
2023-12-02 01:05:37 +0100

    removing session destroy

--- controllers/users.js
+++ controllers/users.js
@@ -35,7 +35,6 @@ module.exports.login = (req, res) => {
 
 module.exports.logout = (req, res) => {
     req.logout((e) => 'callback');
-    req.session.destroy();
     req.flash('success', "Goodbye!");
     res.redirect('/campgrounds');
 }
